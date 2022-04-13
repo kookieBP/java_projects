6f81a869f26d98407d66bc89f87f5aa04161bb93
kookieBP <kookiedeveloper.gmail.com>
2022-04-13 19:09:18 +0900

    Put if statement

--- src/main/java/forLoop.java
+++ src/main/java/forLoop.java
@@ -8,26 +8,33 @@ public class forLoop {
         System.out.println("원하는 정수 <=2에 해당하는 숫자를 입력하세요: ");
         int i = scan.nextInt();
 
-        for (int j = 0; j < i; j++) {
-            for (int k = 0; k < i-j+1; k++) {
-                System.out.print(" ");
+        if (2 < i) {
+            for (int j = 0; j < i; j++) {
+                for (int k = 0; k < i - j + 1; k++) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k < 2 * j + 1; k++) {
+                    System.out.print(".");
+                }
+                System.out.println(" ");
             }
-            for (int k = 0; k < 2 * j + 1; k++) {
-                System.out.print(".");
-            }
-            System.out.println(" ");
-        }
 
-        //첫줄은 점이 다 찍히고 공백은 없고, 마지막줄은 점 하나만 나오게하기!
-        for(int j = 0; j < i+1; j++){
-            for (int k = 0; k < j+1 ; k++){
-                System.out.print(" ");
+            //첫줄은 점이 다 찍히고 공백은 없고, 마지막줄은 점 하나만 나오게하기!
+            for (int j = 0; j < i + 1; j++) {
+                for (int k = 0; k < j + 1; k++) {
+                    System.out.print(" ");
+                }
+                for (int k = -1; k < (i + i - j * 2); k++) {
+                    System.out.print(".");
+                }
+                System.out.println(" ");
             }
-            for (int k = -1; k < (i+i-j*2); k++) {
-                System.out.print(".");
-            }
-            System.out.println(" ");
+
+        }else {
+            System.out.println("다시 재생 하세요!");
         }
 
     }
+
+
 }
